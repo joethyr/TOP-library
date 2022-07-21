857d4db81ec43a48347396195d62deaa08bcabdd
joethyr <joethayr@gmail.com>
2022-07-20 20:48:06 -0600

    create a new book object with user form input

--- js/app.js
+++ js/app.js
@@ -1,31 +1,37 @@
 const showForm = document.querySelector('#showForm');
 const form = document.querySelector('#bookForm');
 let myLibrary = [];
-
-
+let newBook;
 // hide the form
 form.style.display = "none";
-
-// eventlister reveals form
-showForm.addEventListener("click", () => {
-  form.style.display === "none" ? form.style.display = "block" : form.style.display = "none" ;
-});
-
-
-// eventlistener to get user input on form
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-});
-
 
 // Book contructor
 function Book(title, author, pages, read) {
   this.title = title,
   this.author = author,
   this.pages = pages,
-  this.read = read
+  this.read = read;
 }
 
 function addBookToLibrary() {
   // do stuff here
 }
+
+// eventlister reveals form
+showForm.addEventListener("click", () => {
+  form.style.display === "none" ? form.style.display = "block" : form.style.display = "none" ;
+});
+
+// eventlistener to get user input on form
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let formValue = e.target;
+  newBook = new Book(
+    formValue.titleBook.value,
+    formValue.authorBook.value,
+    formValue.pagesBook.value,
+    formValue.readBook.value);
+
+    myLibrary.push(newBook);
+    alert(JSON.stringify(newBook));
+});
