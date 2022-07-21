a110ae10e89e5095cf165d71afb41452a96f4e30
joethyr <joethayr@gmail.com>
2022-07-20 20:15:26 -0600

    add eventlistener fuctions to book form

--- js/app.js
+++ js/app.js
@@ -1,7 +1,25 @@
+const showForm = document.querySelector('#showForm');
+const form = document.querySelector('#bookForm');
 let myLibrary = [];
 
+
+// hide the form
+form.style.display = "none";
+
+// eventlister reveals form
+showForm.addEventListener("click", () => {
+  form.style.display === "none" ? form.style.display = "block" : form.style.display = "none" ;
+});
+
+
+// eventlistener to get user input on form
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+})
+
+
+// Book contructor
 function Book() {
-  // the constructor...
 }
 
 function addBookToLibrary() {
